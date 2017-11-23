8536e3371f9e526248a48e58fea81e9050df0302
NicoZarate <nico.zarate1984@gmail.com>
2017-11-23 19:05:57 -0300

    monitor

---
+++ pages/indicadores.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Monitor;
+
+require '../includes/config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'layout/defaultHead.php'; ?>
+
+</head>
+
+<body>
+	<div id="wrapper">
+        <!-- Navigation -->
+        <?php include 'layout/navbar.php'; ?>
+
+        <h1>Indicadores</h1>
+
+    </div>
+</body>
+</html>
